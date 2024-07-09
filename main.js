0973396bf711d739e709b6a286ed957eadde2943
AdrianGogyan <gogyanadrian@gmail.com>
2024-07-09 16:25:22 +0200

    added fetch function to load a joke

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-import "./styles/main.scss"
+import { fetchJoke } from "./fetching";
+import "./styles/main.scss";
+
+
+// console.log(await fetchJoke());
+
+const loadNewJokeBtn = document.querySelector(".current-joke__generate");
+const currentJokeEl = document.querySelector(".current-joke__text");
+
+async function loadNewJoke() {
+    const joke = await fetchJoke();
+    currentJokeEl.innerText = joke;
+}
+
+loadNewJokeBtn.addEventListener("click", loadNewJoke);
