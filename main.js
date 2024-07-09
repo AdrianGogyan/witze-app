124c96666fbf3c4763678178747e723586c40da5
AdrianGogyan <gogyanadrian@gmail.com>
2024-07-09 16:40:22 +0200

    worked on buttons

--- main.js
+++ main.js
@@ -6,10 +6,27 @@ import "./styles/main.scss";
 
 const loadNewJokeBtn = document.querySelector(".current-joke__generate");
 const currentJokeEl = document.querySelector(".current-joke__text");
+const saveJokeBtn = document.querySelector(".current-joke__save");
+
+let currentJoke = "";
 
 async function loadNewJoke() {
     const joke = await fetchJoke();
+
+    if(!currentJoke){
+        saveJokeBtn.classList.remove("current-joke__save--disabled");
+    }
+
+    currentJoke = joke;
     currentJokeEl.innerText = joke;
+}
+
+
+function saveCurrentJoke(){
+    if(currentJoke){
+        // 1.saveJoke
+        // 2.renderJokes-- saved ones
+    }
 }
 
 loadNewJokeBtn.addEventListener("click", loadNewJoke);
