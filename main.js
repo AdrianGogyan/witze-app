51bbaf13076590b7ff3966b17d3057feb6c0656b
AdrianGogyan <gogyanadrian@gmail.com>
2024-07-26 16:37:49 +0200

    Implement all functions, storing and deleting.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+import { render } from "sass";
 import { fetchJoke } from "./fetching";
+import { getSavedJokes, removeJoke, saveJoke } from "./storing";
 import "./styles/main.scss";
 
 
@@ -7,6 +9,8 @@ import "./styles/main.scss";
 const loadNewJokeBtn = document.querySelector(".current-joke__generate");
 const currentJokeEl = document.querySelector(".current-joke__text");
 const saveJokeBtn = document.querySelector(".current-joke__save");
+
+const savedJokesListEl = document.querySelector(".saved-jokes__list");
 
 let currentJoke = "";
 
@@ -24,9 +28,48 @@ async function loadNewJoke() {
 
 function saveCurrentJoke(){
     if(currentJoke){
-        // 1.saveJoke
-        // 2.renderJokes-- saved ones
+        saveJoke(currentJoke);
+        renderSavedJokes();
     }
 }
 
+function removeSavedJoke(index){
+    removeJoke(index);
+    renderSavedJokes();
+}
+
+window.removeSavedJoke = removeSavedJoke;
+
+function renderSavedJokes(){
+    const savedJokes = getSavedJokes();
+
+    let html = "";
+
+    savedJokes.forEach((joke, index) => {
+        html += 
+        `
+        <div class="saved-joke">
+            <div class="saved-joke__text">
+                ${joke}
+            </div>
+            <button class="saved-joke__remove" onclick="removeSavedJoke(${index})">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="saved-joke__remove-icon">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m3 3 1.664 1.664M21 21l-1.5-1.5m-5.485-1.242L12 17.25 4.5 21V8.742m.164-4.078a2.15 2.15 0 0 1 1.743-1.342 48.507 48.507 0 0 1 11.186 0c1.1.128 1.907 1.077 1.907 2.185V19.5M4.664 4.664 19.5 19.5" />
+              </svg>
+              
+            </button>
+        </div>
+        `;
+    });
+
+    if(!html) html = "<em>Noch keine Witze gespeichert.</em>";
+
+
+    savedJokesListEl.innerHTML = html;
+
+}
+
 loadNewJokeBtn.addEventListener("click", loadNewJoke);
+saveJokeBtn.addEventListener("click", saveCurrentJoke);
+
+renderSavedJokes();
